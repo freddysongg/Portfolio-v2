0634b818f3131b2d306cbb3b08bab7fa01cf0fb2
Freddy Song <fredsong99@gmail.com>
2025-01-14 15:25:25 -0800

    v3 build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,26 @@
-export default function Home() {
+import React from 'react';
+import Sidebar from '../components/Contact'
+import Hero from '../components/Hero';
+import Projects from '../components/Projects';
+import WorkExperience from '../components/WorkExperience';
+import Contact from '../components/Contact';
+import { Analytics } from '@vercel/analytics/next';
+
+const App = () => {
   return (
-    <div>
-      <h1>Welcome to My Next.js App</h1>
+    <div className="flex flex-col md:flex-row min-h-screen">
+      <Analytics />
+      <Sidebar />
+      <main className="flex-1 p-8 md:ml-64">
+        <div className="max-w-4xl mx-auto space-y-24">
+          <Hero />
+          <Projects />
+          <WorkExperience />
+          <Contact />
+        </div>
+      </main>
     </div>
   );
-}
+};
+
+export default App;
