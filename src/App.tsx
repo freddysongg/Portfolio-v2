a80b30a17ffab8321c9eba483d87ba3800c52dcc
Freddy Song <fredsong99@gmail.com>
2025-01-14 16:32:14 -0800

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Hero from './components/Hero';
 import Projects from './components/Projects';
 import WorkExperience from './components/WorkExperience';
 import Contact from './components/Contact';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="flex flex-col md:flex-row min-h-screen">
+      <Analytics/>
       <Sidebar />
       <main className="flex-1 p-8 md:ml-64">
         <div className="max-w-4xl mx-auto space-y-24">
